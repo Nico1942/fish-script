5f0c7e6c8f7ccabc22031c71d7f1d6ab41cbec19
Nicolás Cualangelo <ncualangelo@gmail.com>
2023-11-17 00:40:06 -0300

    Modificado el punto de yip, agregado un logo más chico a ext-fetch

--- ext-fetch.fish
+++ ext-fetch.fish
@@ -1,55 +1,64 @@
 function ext-fetch --description="Show all gnome extensions enabled."
 
-  # Space between logo and text
-  set space "      "
+    # Space between logo and text
+    set space "     "
 
-  set logo[1]   " ⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀⢀⣤⣶⣶⣦"
-  set logo[2]   " ⠀⠀⠀⣴⣶⠀⢺⣿⡇⠀⣾⣿⣿⣿⠃"
-  set logo[3]   " ⢠⣶⡄⠹⠿⠀⠈⠋⠀⠀⠻⠿⠛⠁⠀"
-  set logo[4]   " ⠈⠻⠇⠀⣠⣤⣶⣶⣾⣷⣶⣦⡀⠀⠀"
-  set logo[5]   " ⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀"
-  set logo[6]   " ⠀⠀⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀"
-  set logo[7]   " ⠀⠀⢿⣿⣿⣿⡿⠋⠁⢀⣠⣤⡀⠀⠀"
-  set logo[8]   " ⠀⠀⠀⠻⣿⣿⣷⣄⣀⣼⣿⣿⠇⠀⠀"
-  set logo[9]   " ⠀⠀⠀⠀⠈⠛⠿⠿⣿⠿⠛⠁⠀⠀⠀"
-  set logo[10]  "                "
-  set logoWidth "                "
+    set logo[1] "⠀⠀⠀⠀⠀⣤⣄⠀⢀⣴⣶⣶"
+    set logo[2] "⠀⠀⣾⣆⠘⣿⡏⠀⣿⣿⣿⠏"
+    set logo[3] "⣿⡆⠙⠃⠀⣈⣀⣀⡙⠋⠁⠀"
+    set logo[4] "⠈⢁⣤⣾⣿⣿⣿⣿⣿⣷⠀⠀"
+    set logo[5] "⠀⣾⣿⣿⣿⣿⣿⣿⠿⠃⠀⠀"
+    set logo[6] "⠀⢿⣿⣿⣿⡿⠋⠁⣀⣀⠀⠀"
+    set logo[7] "⠀⠈⢿⣿⣿⣄⣀⣼⣿⡿⠁⠀"
+    set logo[8] "⠀⠀⠀⠉⠛⠿⠿⠿⠛⠁⠀⠀"
+    set logoWidth "           "
 
-  set extensions (gnome-extensions list --enabled | cut -d @ -f 1 | sort)
-  set totalExtensions (count $extensions)
- # set all (gnome-extensions list | cut -d @ -f1)
- # set allnro (count $all)
+    #set logo[1]   " ⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀⢀⣤⣶⣶⣦"
+    #set logo[2]   " ⠀⠀⠀⣴⣶⠀⢺⣿⡇⠀⣾⣿⣿⣿⠃"
+    #set logo[3]   " ⢠⣶⡄⠹⠿⠀⠈⠋⠀⠀⠻⠿⠛⠁⠀"
+    #set logo[4]   " ⠈⠻⠇⠀⣠⣤⣶⣶⣾⣷⣶⣦⡀⠀⠀"
+    #set logo[5]   " ⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀"
+    #set logo[6]   " ⠀⠀⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀"
+    #set logo[7]   " ⠀⠀⢿⣿⣿⣿⡿⠋⠁⢀⣠⣤⡀⠀⠀"
+    #set logo[8]   " ⠀⠀⠀⠻⣿⣿⣷⣄⣀⣼⣿⣿⠇⠀⠀"
+    #set logo[9]   " ⠀⠀⠀⠀⠈⠛⠿⠿⣿⠿⠛⠁⠀⠀⠀"
+    #set logo[10]  "                "
+    #set logoWidth "                "
+    #
+    set extensions (gnome-extensions list --enabled | cut -d @ -f 1 | sort)
+    set totalExtensions (count $extensions)
+    # set all (gnome-extensions list | cut -d @ -f1)
+    # set allnro (count $all)
 
-  # Setup title
-  set logo[1] "$logo[1]$space\033[1;37mextensions [$totalExtensions]" 
-  set logo[2] "$logo[2]$space\b\b\033[1;37m├──────────────────────────────┤"
+    # Setup title
+    set logo[1] "$logo[1]$space\033[1;37mextensions [$totalExtensions]"
+    set logo[2] "$logo[2]$space\b\b\b\033[1;37m├──────────────────────────────┤"
 
-  set logoHeight (count $logo)
-  set index 3
+    set logoHeight (count $logo)
+    set index 3
 
-  for i in $extensions
-    if test $index -le $logoHeight
-      set logo[$index] "$logo[$index]$space• $i"
-      set index (math $index + 1)
-    else
-      set logo[$index] "$logoWidth$space• $i"
-      set index (math $index + 1)
+    for i in $extensions
+        if test $index -le $logoHeight
+            set logo[$index] "$logo[$index]$space• $i"
+            set index (math $index + 1)
+        else
+            set logo[$index] "$logoWidth$space• $i"
+            set index (math $index + 1)
+        end
     end
-  end
 
 
-  echo -e "\n"
-  echo -e $logoWidth$space\b\b"┌──────────────────────────────┐" | lolcat -b
+    echo -e "\n"
+    echo -e $logoWidth$space\b\b"┌──────────────────────────────┐" | lolcat -b
 
-  for i in $logo
-    sleep 0.001
-    if test "$i" = "$logo[2]"
-      echo -e $i | lolcat -b
-      continue
+    for i in $logo
+        sleep 0.001
+        if test "$i" = "$logo[2]"
+            echo -e $i | lolcat -b
+            continue
+        end
+        echo -e $i | sed 's/-/ /g' | string shorten -m 50 | lolcat -b
     end
-    echo -e $i | sed 's/-/ /g' | string shorten -m 50 | lolcat -b
-  end
 
-  echo -e $logoWidth$space\b\b"└──────────────────────    ─┘" | lolcat -b
+    echo -e $logoWidth$space\b\b"└──────────────────────    ─┘" | lolcat -b
 end
-
--- yip.fish
+++ yip.fish
@@ -1,45 +1,45 @@
 function yip
 
-  # Set up
-  ## UI
-  set FRONT_COLOR 11cf18
-  set BACK_COLOR 8c4646
-  set TIME_SLOW 0.3
-  set TIME_FAST 0.07
+    # Set up
+    ## UI
+    set FRONT_COLOR 11cf18
+    set BACK_COLOR 8c4646
+    set TIME_SLOW 0.3
+    set TIME_FAST 0.07
 
-  if test "$argv" = "-no-time"
-    set TIME_FAST 0
-    set TIME_SLOW 0
-  end
+    if test "$argv" = -no-time
+        set TIME_FAST 0
+        set TIME_SLOW 0
+    end
 
-  set HOY (date +%j)
-  set CIEN (date +%j -d "Dec 31")
-  set PORCENTAJE (math -s0 (math 100/$CIEN) \* $HOY)
+    set HOY (date +%j)
+    set CIEN (date +%j -d "Dec 31")
+    set PORCENTAJE (math -s0 (math 100/$CIEN) \* $HOY)
 
-  set_color -o
-  set_color white
-  echo "Progreso anual:"
+    set_color -o
+    set_color white
+    echo "Progreso anual:"
 
-  set BARRA (math -s0 $PORCENTAJE / 10)
+    set BARRA (math -s0 $PORCENTAJE / 10)
 
-  set_color -o $FRONT_COLOR
-  for i in (seq 1 $BARRA)
+    set_color -o $FRONT_COLOR
+    for i in (seq 1 $BARRA)
+        sleep $TIME_SLOW
+        echo -en "•"
+        #    echo -en "⣿"
+    end
+
     sleep $TIME_SLOW
-    echo -en "o"
-#    echo -en "⣿"
-  end
 
-  sleep $TIME_SLOW
+    set_color $BACK_COLOR
+    for i in (seq 1 (math 10 - $BARRA))
+        sleep $TIME_FAST
+        echo -en •
+    end
 
-  set_color $BACK_COLOR
-  for i in (seq 1 (math 10 - $BARRA))
     sleep $TIME_FAST
-    echo -en "o"
-  end
-
-  sleep $TIME_FAST 
-  set_color -o $FRONT_COLOR
-  echo " $PORCENTAJE%"
-  sleep $TIME_SLOW 
+    set_color -o $FRONT_COLOR
+    echo " $PORCENTAJE%"
+    sleep $TIME_SLOW
 
 end
