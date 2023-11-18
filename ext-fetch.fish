422a88cde09cb60e96ca6d3d9b8cf5d36f4823af
Nicolás Cualangelo <ncualangelo@gmail.com>
2023-11-17 21:30:41 -0300

    Agregada la opción sin logo y mostar todas las extensiones instaladas

--- ext-fetch.fish
+++ ext-fetch.fish
@@ -13,25 +13,25 @@ function ext-fetch --description="Show all gnome extensions enabled."
     set logo[8] "⠀⠀⠀⠉⠛⠿⠿⠿⠛⠁⠀⠀"
     set logoWidth "            "
 
-    #set logo[1]   " ⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀⢀⣤⣶⣶⣦"
-    #set logo[2]   " ⠀⠀⠀⣴⣶⠀⢺⣿⡇⠀⣾⣿⣿⣿⠃"
-    #set logo[3]   " ⢠⣶⡄⠹⠿⠀⠈⠋⠀⠀⠻⠿⠛⠁⠀"
-    #set logo[4]   " ⠈⠻⠇⠀⣠⣤⣶⣶⣾⣷⣶⣦⡀⠀⠀"
-    #set logo[5]   " ⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀"
-    #set logo[6]   " ⠀⠀⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀"
-    #set logo[7]   " ⠀⠀⢿⣿⣿⣿⡿⠋⠁⢀⣠⣤⡀⠀⠀"
-    #set logo[8]   " ⠀⠀⠀⠻⣿⣿⣷⣄⣀⣼⣿⣿⠇⠀⠀"
-    #set logo[9]   " ⠀⠀⠀⠀⠈⠛⠿⠿⣿⠿⠛⠁⠀⠀⠀"
-    #set logo[10]  "                "
-    #set logoWidth "                "
-    #
+    set allExtensions false
+    for i in $argv
+        if test "$i" = --all
+            set extensionsDisabled (gnome-extensions list --disabled | cut -d @ -f1 | sort | string lower)
+            set numDisabled (count $extensionsDisabled)
+            set allExtensions true
+        end
+        if test "$i" = --off
+            set logo ""
+            set logoWidth ""
+            set space "  "
+        end
+    end
+
     set extensions (gnome-extensions list --enabled | cut -d @ -f 1 | sort)
-    set totalExtensions (count $extensions)
-    # set all (gnome-extensions list | cut -d @ -f1)
-    # set allnro (count $all)
+    set numExtensions (count $extensions)
 
     # Setup title
-    set logo[1] "$logo[1]$space\033[1;37mextensions [$totalExtensions]"
+    set logo[1] "$logo[1]$space\033[1;37mextensions [$numExtensions]"
 
     set logoHeight (count $logo)
     set index 2
@@ -46,6 +46,22 @@ function ext-fetch --description="Show all gnome extensions enabled."
         end
     end
 
+    if test "$allExtensions" = true
+        set index (math $index + 1)
+        set logo[$index] "$logo[$index]$space\033[1;37mdisabled [$numDisabled]"
+        set index (math $index + 1)
+
+        for i in $extensionsDisabled
+            if test $index -le $logoHeight
+                set logo[$index] "$logo[$index]$space• $i"
+                set index (math $index + 1)
+            else
+                set logo[$index] "$logoWidth$space• $i"
+                set index (math $index + 1)
+            end
+        end
+    end
+
     if test $index -le $logoHeight
         set logoWidth ""
     end
@@ -54,10 +70,6 @@ function ext-fetch --description="Show all gnome extensions enabled."
 
     for i in $logo
         sleep 0.001
-        if test "$i" = "$logo[2]"
-            echo -e $i | lolcat -b
-            continue
-        end
         echo -e $i | sed 's/-/ /g' | string shorten -m 50 | lolcat -b
     end
 
