8e88087c9facc767cd74bde6fb1ab7f7cdbf4e71
Nicolás Cualangelo <ncualangelo@gmail.com>
2023-12-28 18:34:59 -0300

    Mejorada miWall

--- miWall.fish
+++ miWall.fish
@@ -1,28 +1,28 @@
-# Defined in - @ line 15
+# Defined in - @ line 17
 
-set RESOLUTION (xdpyinfo | awk '/dimensions:/ { print $2 }')
+set RESOLUTION (miResolucion)
 
 function ayuda
   echo "Se debe ingresar primero el archivo a editar y luego el nombre del archivo editado."
   echo "Si no se ingresa un nombre de salida, el archivo tendrá el sufijo '_mod'."
-  echo "Tamaño de pantalla: $RESOLUTION (16:10)"
+  echo "Tamaño de pantalla: $RESOLUTION"
 end
 
 function unArgumento
-  set FILE (echo $argv[1] | cut -d '.' -f1)
-  set EXT (echo $argv[1] | cut -d '.' -f2)
+  set FILE (echo $argv | cut -d '.' -f1)
+  set EXT (echo $argv | cut -d '.' -f2)
   set OUTPUT (echo $FILE'_mod.'$EXT)
-  convert $argv[1] -resize $RESOLUTION $OUTPUT
+  convert $argv -resize $RESOLUTION $OUTPUT
 end
 
 function miWall
-  if test "$argv[1]" = ""
-    ayuda
-  else
-    if test "$argv[2]" = ""
-      unArgumento $argv[1]
-    else
+  if test (count $argv) = 2
       convert $argv[1] -resize $RESOLUTION $argv[2];
+  else
+    if test (count $argv) = 1
+      unArgumento $argv
+    else
+      ayuda
     end
   end
 end
