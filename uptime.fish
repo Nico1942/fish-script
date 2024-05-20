7a2561588f2b51bc7854050a077b9245b8436e26
Nicolás Cualangelo <ncualangelo@gmail.com>
2024-05-20 11:56:01 -0300

    Fix typo

--- uptime.fish
+++ uptime.fish
@@ -1,3 +1,3 @@
-function uptime --wraps='4h 4m' --description 'A minimal uptime. Ex.: 1h 2m 3m' 
+function uptime --wraps='4h 4m' --description 'A minimal uptime. Ex.: 1h 2m 3s' 
  command uptime -p | sed 's/up //;  s/ day/d/;s/ hour/h/; s/,//g; s/ minute/m/; s/s//g'; 
 end
