586b9fb9ce09ff04d83761f69bded7086dbdc0b3
Nicolás Cualangelo <ncualangelo@gmail.com>
2024-05-28 12:29:40 -0300

    Add info to show.

--- perfil-ext.fish
+++ perfil-ext.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set option "good-looking" "minimal" "no"
+set option "good-looking" "minimal" "minimal2" "no"
 
 function help
    echo "
@@ -14,8 +14,11 @@ function disableAll
     set -l extensions (gnome-extensions list --enabled)
 
     for ext in $extensions
+        echo "Desactivando $(nombrar $ext)"
 	gnome-extensions disable $ext
+	sleep 0.05
     end
+    echo "--- ||| ---"
 end
 
 
@@ -27,18 +30,23 @@ function needed
     return true
   else
     return false
-    echo " Faltan extensiones.
- Necesarias: $argv"
   end
+end
+
+function nombrar
+   set -l nombre (echo $argv | sed 's/\@.*//g; s/-/ /g')
+   echo $nombre
 end
 
 
 function goodLooking
-    set -l extensions "arcmenu@arcmenu.com" "awesome-tiles@velitasali.com" "blur-my-shell@aunetx" "compiz-windows-effect@hermes83.github.com" "just-perfection-desktop@just-perfection" "pano@elhan.io"
+    set -l extensions "arcmenu@arcmenu.com" "awesome-tiles@velitasali.com" "blur-my-shell@aunetx" "compiz-windows-effect@hermes83.github.com" "just-perfection-desktop@just-perfection" "pano@elhan.io" "user-theme@gnome-shell-extensions.gcampax.github.com"
 
     if [ needed $extesions ]
        for ext in $extensions
+	  echo "Activando $(nombrar $ext)"
 	  gnome-extensions enable $ext
+	  sleep 0.05
        end
     end
 
@@ -48,10 +56,21 @@ function minimal
     set -l extensions "awesome-tiles@velitasali.com" "just-perfection-desktop@just-perfection" "pano@elhan.io"
 
     for ext in $extensions
+	echo "Activando $(nombrar $ext)"
 	gnome-extensions enable $ext
+	sleep 0.05
     end
 end
 
+function minimal2
+    set -l extensions "compiz-windows-effect@hermes83.github.com" "user-theme@gnome-shell-extensions.gcampax.github.com"
+    minimal
+    for ext in $extensions
+	echo "Activando $(nombrar $ext)"
+	gnome-extensions enable $ext
+	sleep 0.05
+    end
+end
 
 if contains $argv $option
   disableAll
@@ -60,6 +79,8 @@ if contains $argv $option
 	    goodLooking
     case "minimal"
 	minimal
+    case "minimal2"
+	minimal2
     case "no"
       disableAll
   end
