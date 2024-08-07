97bc132b228d04943eae65f88e10f9713772eacf
Nicolás Cualangelo <ncualangelo@gmail.com>
2024-08-07 11:02:37 -0300

    Activar/Desactivar extensión de bluetooth.

---
+++ bluetooth.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+set estado (gnome-extensions info Bluetooth-Battery-Meter@maniacx.github.com | grep 'Estado: ' | sed 's/  Estado: //g')
+
+if [ "$estado" = "ACTIVE" ]
+  gnome-extensions disable Bluetooth-Battery-Meter@maniacx.github.com
+  echo "Desactivando extensión."
+else
+  gnome-extensions enable Bluetooth-Battery-Meter@maniacx.github.com
+  echo "Activando extensión."
+end
