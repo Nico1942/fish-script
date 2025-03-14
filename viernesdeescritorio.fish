78d36463e942a3fd39b9d52bb5d58ae7d3fa0b4e
Nico1942 <ncualangelo@gmail.com>
2025-03-14 12:07:32 -0300

    Optimización

--- viernesdeescritorio.fish
+++ viernesdeescritorio.fish
@@ -10,31 +10,34 @@ function viernesdeescritorio
         set captura true
     end
 
-    set arg1 (contains -- --off $argv: and echo "--off")
-    set arg2 (contains -- --all $argv: and echo "--all")
+    set arg1 (contains -- --off $argv; and echo "--off")
+    set arg2 (contains -- --all $argv; and echo "--all")
 
+    # Ayuda memoria.
     if contains -- --help -h $argv || contains -- -h $argv
       echo "Ayuda: 
       -c:    Limpia la pantalla antes
       -s:    Hace captura de pantalla
       --off: Sin logos
-      --all: Mustra también las extensiones instaladas"
-    else
-
-      yasfetch $arg1
-      ext-fetch $arg1 $arg2
-      echo
-      yip -no-time
-      echo
-      echo "#"$dia"DeEscritorio :D" | lolcat -b
-      echo
-
-      if $captura
-        mkdir -p (xdg-user-dir PICTURES)/Escritorios
-        set archivo (xdg-user-dir PICTURES)/Escritorios/$fecha.png
-        gnome-screenshot -p -f "$archivo"
-        flatpak run org.gnome.Loupe $archivo &>/dev/null &
-      end
-
+      --all: Muestra las extensiones desactivadas"
+      return
     end
+
+    # Comandos principales
+
+    yasfetch $arg1
+    ext-fetch $arg1 $arg2
+    echo
+    yip -no-time
+    echo
+    echo "#"$dia"DeEscritorio :D" | lolcat -b
+    echo
+
+    if $captura
+      mkdir -p (xdg-user-dir PICTURES)/Escritorios
+      set archivo (xdg-user-dir PICTURES)/Escritorios/$fecha.png
+      gnome-screenshot -p -f "$archivo"
+      loupe $archivo &>/dev/null &
+    end
+
 end
