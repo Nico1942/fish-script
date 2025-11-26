bd8f7a10dcc3a814da6e7eae945ab702fc3f9ce4
Nico1942 <ncualangelo@gmail.com>
2025-11-26 09:41:43 -0300

    Agregado opciones de nuevos editores.

--- tedit.fish
+++ tedit.fish
@@ -1,3 +1,16 @@
 function tedit --wraps='cd ~/Documentos/TODO.TXT/;n todo.txt' --description 'Editar todo.txt con editor de texto'
-  subl $HOME/Documentos/TODO.TXT/todo.txt $argv
+	set file "$HOME/Documentos/TODO.TXT/todo.txt"
+
+	switch $argv
+		case "-s"
+			subl $file
+		case "-g"
+			gnome-text-editor $file
+		case "-v"
+			nvim $file
+		case "*"
+			echo "-s: sublime text"
+			echo "-g: gnome text editor"
+			echo "-v: neovim"
+	end
 end
