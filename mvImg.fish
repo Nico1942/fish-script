d746073c452ea671850a900073930b4d40030577
Nico1942 <ncualangelo@gmail.com>
2026-02-03 18:40:19 -0300

    Fix folder bug.

--- mvImg.fish
+++ mvImg.fish
@@ -1,5 +1,5 @@
 function mvImg --wraps='mv {*.png, *.jpeg, *.jpg} ./img/' --description 'alias mvImg mv {*.png, *.jpeg, *.jpg} ./img/'
-	cd $(xdg-user-dir PICTURES)
+	cd $(xdg-user-dir DOWNLOAD)
 	mv {*.png, *.jpeg, *.jpg} ./img/ $argv
 
 end
--- mvVid.fish
+++ mvVid.fish
@@ -1,5 +1,5 @@
 function mvVid --wraps='mv *.mp4 ./Videos/' --description 'alias mvVid mv *.mp4 ./Videos/'
-	cd $(xdg-user-dir VIDEOS)
-	mv *.mp4 ./Videos/ $argv
+	cd $(xdg-user-dir DOWNLOAD)
+	mv *.mp4 Videos/ $argv
 
 end
